c2818ecd373864914ed9c4a04b5636331627e48a
Jaime <javial@floridauniversitaria.com>
2023-12-09 09:16:13 +0100

    restarting project parallax

---
+++ my-react-app/src/components/HomePage.js
@@ -0,0 +1,14 @@
+// Import necessary libraries
+import React from 'react';
+
+// App component
+function HomePage() {
+  return (
+    <div className="App">
+      {/* Other components or content */}
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default HomePage;
